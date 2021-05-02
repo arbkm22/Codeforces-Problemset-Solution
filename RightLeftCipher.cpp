2a97fa3910f673b9c2dc0a781d715fffd76af774
Bhaskar Mahto <arbkm22@gmail.com>
2021-05-02 23:56:58 +0530

    #1085A - Right-Left Cipher

---
+++ RightLeftCipher.cpp
@@ -0,0 +1,58 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author      : hellking
+File        : RightLeftCipher
+Created on  : Sun, 02 May, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string s, ans;   
+    cin >> s;
+    if (s.size()==1) {
+        cout << s << endl;
+        return 0;
+    }
+    int f = (s.size()/2)-1;
+    int right = f+1;
+    int left = f-1;
+    ans += s[f];
+    for (int i=0; i<s.size()-1; i++) {
+        if (i%2==0) {
+            ans.push_back(s[right]);
+            right++;
+        }
+        else {
+            ans.push_back(s[left]);
+            left--;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}

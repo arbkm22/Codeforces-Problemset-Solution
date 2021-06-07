743d55ba280b7503f49da9369b12bfcfe2dec794
Bhaskar Mahto <arbkm22@gmail.com>
2021-06-07 21:02:18 +0530

    #1209A - Paint the Numbers

---
+++ PaintNumbers.cpp
@@ -0,0 +1,64 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author      : hellking
+File        : PaintNumbers
+Created on  : Mon, 07 June, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    bool state = false;
+    vector<int> a(n);
+    for (auto &x:a) {
+        cin >> x;
+        if (x==1)
+            state = true;
+    }
+    sort(a.begin(), a.end());
+    for (int i=0; i<a.size(); i++) {
+        for (int j=i+1; j<a.size(); j++) {
+            if (a[j]%a[i]==0)
+                a.erase(a.begin()+j);
+        }
+    }
+    for (int i=0; i<a.size(); i++) {
+        for (int j=i+1; j<a.size(); j++) {
+            if (a[j]%a[i]==0)
+                a.erase(a.begin()+j);
+        }
+    }
+    if (state)
+        cout << 1 << endl;
+    else
+        cout << a.size() << endl;
+
+    return 0;
+}

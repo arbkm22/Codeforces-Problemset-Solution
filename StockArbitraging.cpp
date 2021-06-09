b9f17df30f7b46348172722ac50cb308955716d2
Bhaskar Mahto <arbkm22@gmail.com>
2021-06-09 17:22:39 +0530

    #1150A - Stock Arbitraging

---
+++ StockArbitraging.cpp
@@ -0,0 +1,53 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author      : hellking
+File        : StockArbitraging
+Created on  : Wed, 09 June, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m, r;
+    cin >> n >> m >> r;
+    vector<int> a(n), b(m);
+    for (auto &x:a)
+        cin >> x;
+    for (auto &y:b)
+        cin >> y;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    if (a[0] < b[m-1]) {
+        int tmp = r/a[0];
+        r -= (tmp*a[0]);
+        r += (tmp*b[m-1]);
+    }   
+    cout << r << endl;
+
+    return 0;
+}

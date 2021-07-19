d7724afc9059a31339af5b60dcc2cd9425beeef6
Bhaskar <arbkm22@gmail.com>
2021-07-19 09:59:09 +0530

    1141A - Game 23

---
+++ Game23.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    long long n, m;
+    cin >> n >> m;
+    int result = -1;
+    if (m%n==0) {
+        result = 0;
+        int d = m/n;
+        while (d%2==0) {
+            d /= 2;
+            result++;
+        }
+        while (d%3==0) {
+            d /= 3;
+            result++;
+        }
+        if (d!=1)
+            result = -1;
+    }
+    cout << result << endl;
+
+    return 0;
+}

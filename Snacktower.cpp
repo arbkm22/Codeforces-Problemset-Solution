a27780b623b3dc62a5d58f8847a4e601fb438e10
Bhaskar <arbkm22@gmail.com>
2022-04-22 23:21:10 +0530

    #767A - Snacktower

---
+++ Snacktower.cpp
@@ -0,0 +1,62 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  '9XXXXXXXXXXXXXXXXXXXXX'~   ~'OOO8b   d8OOO'~   ~'XXXXXXXXXXXXXXXXXXXXXP'
+    '9XXXXXXXXXXXP' '9XX'   DIE    '98v8P'  HUMAN   'XXP' '9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP''v''9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   '9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   'XXXXXb.dX|Xb.dXXXXX'   'dXXP
+                     ''      9XXXXXX(   )XXXXXXP      ''
+                              XXXX X.'v'.X XXXX
+                              XP^X''b   d''X^XX
+                              X. 9  '   '  P )X
+                              'b  '       '  d'
+                               '             '
+ 
+Author      : hellking
+File        : Snacktower
+Created on  : Mon Apr 18 09:38:54 PM IST 2022
+*/
+
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int visited[100000];
+    int arr[1000000];
+    memset(visited, false, sizeof(visited));
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++) 
+        cin >> arr[i];
+    int maxEle = n;
+    for (int i=1; i<=n; i++) {
+        int x = arr[i];
+        visited[x] = true;
+        if (x == maxEle) {
+            while (visited[maxEle]) {
+                cout << maxEle << " ";
+                maxEle--;
+            }
+            cout << endl;
+        }
+        else {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}

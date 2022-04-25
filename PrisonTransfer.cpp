9551ef0f0f6965b21b1e87631d34d0f31d67403c
Bhaskar <arbkm22@gmail.com>
2022-04-25 21:04:03 +0530

    #427B - Prison Transfer

---
+++ PrisonTransfer.cpp
@@ -0,0 +1,54 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  '9XXXXXXXXXXXXXXXXXXXXX'~   ~'OOO8b   d8OOO'~   ~'XXXXXXXXXXXXXXXXXXXXXP'
+    '9XXXXXXXXXXXP' '9XX'   DIE    '98v8P'  HUMAN   'XXP' '9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP''v''9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   '9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   'XXXXXb.dX|Xb.dXXXXX'   'dXXP
+                     ''      9XXXXXX(   )XXXXXXP      ''
+                              XXXX X.'v'.X XXXX
+                              XP^X''b   d''X^XX
+                              X. 9  '   '  P )X
+                              'b  '       '  d'
+                               '             '
+ 
+Author      : hellking
+File        : PrisonTransfer
+Created on  : Mon Apr 25 04:19:52 PM IST 2022
+*/
+
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, t, c;
+    cin >> n >> t >> c;
+    vector<int> arr(n);
+    for (auto &x:arr)
+        cin >> x;
+    int count = 0, pcount = 0;
+    for (int i=0; i<n; i++) {
+        if (arr[i] <= t) 
+            count++;
+        else 
+            count = 0;
+        if (count >= c)
+            pcount++;
+    }
+    cout << pcount << endl;
+
+    return 0;
+}

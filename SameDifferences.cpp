7ee98248f1581545cf472279da48eefd35e174e8
Bhaskar <arbkm22@gmail.com>
2022-05-02 12:36:44 +0530

    #1520D - Same Differences

---
+++ SameDifferences.cpp
@@ -0,0 +1,55 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  '9XXXXXXXXXXXXXXXXXXXXX'~   ~'OOO8b   d8OOO'~   ~'XXXXXXXXXXXXXXXXXXXXXP'
+    '9XXXXXXXXXXXP' '9XX'   DIE    '98v8P'  HUMAN   'XXP' '9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP''v''9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   '9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   'XXXXXb.dX|Xb.dXXXXX'   'dXXP
+                     ''      9XXXXXX(   )XXXXXXP      ''
+                              XXXX X.'v'.X XXXX
+                              XP^X''b   d''X^XX
+                              X. 9  '   '  P )X
+                              'b  '       '  d'
+                               '             '
+ 
+Author      : hellking
+File        : SameDifferences
+Created on  : Mon May  2 12:30:40 PM IST 2022
+*/
+
+#include <bits/stdc++.h>
+#include <mutex>
+#include <unordered_map>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        unordered_map<int, int> mp;
+        ll res = 0;
+        for (int i=0; i<n; i++) {
+            int x;
+            cin >> x;
+            x -= i;
+            res += mp[x];
+            mp[x]++;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}

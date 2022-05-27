4d61fe2e873e08f2252b30ccb73e11072aa4a4f1
Bhaskar <arbkm22@gmail.com>
2022-05-27 22:21:42 +0530

    1598C - Delete Two Elements

---
+++ DeleteTwoElements.cpp
@@ -0,0 +1,69 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  '9XXXXXXXXXXXXXXXXXXXXX'~   ~'OOO8b   d8OOO'~   ~'XXXXXXXXXXXXXXXXXXXXXP'
+    '9XXXXXXXXXXXP' '9XX'   DIE    '98v8P'  HUMAN   'XXP' '9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP''v''9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   '9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   'XXXXXb.dX|Xb.dXXXXX'   'dXXP
+                     ''      9XXXXXX(   )XXXXXXP      ''
+                              XXXX X.'v'.X XXXX
+                              XP^X''b   d''X^XX
+                              X. 9  '   '  P )X
+                              'b  '       '  d'
+                               '             '
+ 
+Author      : hellking
+File        : DeleteTwoElements
+Created on  : Fri May 27 10:12:10 PM IST 2022
+*/
+
+#include <bits/stdc++.h>
+#include <map>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        map<int, int> cnt;
+        for (auto &x: arr) {
+            cin >> x;
+            cnt[x]++;
+        }
+        ll sum = accumulate(arr.begin(), arr.end(), 0LL);
+        if ((2 * sum) % n != 0) {
+            cout << 0 << endl;
+            continue;
+        }
+        ll need = (2 * sum)/n;
+        ll ans = 0;
+        for (int i=0; i<n; i++) {
+            int a1 = arr[i];
+            int a2 = need - a1;
+            if (cnt.count(a2))
+                ans += cnt[a2];
+            if (a1 == a2)
+                ans -= 1;
+        }
+        cout << ans/2 << endl;
+    }
+
+    return 0;
+}
